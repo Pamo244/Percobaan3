3a06432d288687893ab25dfea69342ef05fbbc4c
Pamo244 <andrikurniawan68310@gmail.com>
2023-12-18 21:16:51 +0800

    Initial commit

---
+++ app/src/main/java/com/f55122028/percobaan3/Hewan.java
@@ -0,0 +1,9 @@
+package com.f55122028.percobaan3;
+
+public interface Hewan{
+    String respirasi = "oksigen";
+    public void makan();
+
+
+}
+
---
+++ app/src/main/java/com/f55122028/percobaan3/Kucing.java
@@ -0,0 +1,9 @@
+package com.f55122028.percobaan3;
+
+public class Kucing implements Hewan{
+
+    @Override
+    public void makan(){
+
+    }
+}
